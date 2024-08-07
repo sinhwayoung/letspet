235e3d43b9ea430b9887d0c0e079aa6fee21f66d
sinhwayoung <hwa02ya@gmail.com>
2024-08-07 12:13:29 +0900

    배포준비

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+//slidebar
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollContainer = document.getElementById("scrollContainer");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  scrollContainer.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+  });
+
+  scrollContainer.addEventListener("mouseleave", () => {
+    isDown = false;
+  });
+
+  scrollContainer.addEventListener("mouseup", () => {
+    isDown = false;
+  });
+
+  scrollContainer.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // 스크롤 속도 조절을 위한 계수
+    scrollContainer.scrollLeft = scrollLeft - walk;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
